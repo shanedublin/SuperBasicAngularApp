11c866afad1a9a518d59b247c0721068054d3774
Shane Dublin <shanedublin27@gmail.com>
2016-07-05 17:43:07 -0700

    added a few comments

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 (function(){
   'use-strict';
 
+  // create a madule name 'app' attach a controler to it called 'mainController'
   angular.module('app',[]).controller('mainController',function(){
 
       var vm = this;
       vm.counter = 0;
       vm.input = "Hello Angular";
+
+      // adds to a counter
       vm.counterFunction = function(){
         console.log("counter Incremented");
         vm.counter ++;
       }
+
+      // shows an alert when called
       vm.showInput = function(){
         console.log("hello");
         alert(vm.input);
